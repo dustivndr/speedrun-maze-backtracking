2b8a08fea323c691aa1f950033a6ec5bfa7d3e60
Dustin Ivander <dustinduck@duck.com>
2026-06-01 21:10:40 +0700

    Create SpikeAssets.java

---
+++ src/main/java/io/github/maze/obstacles/SpikeAssets.java
@@ -0,0 +1,47 @@
+package io.github.maze.obstacles;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+
+import java.util.Objects;
+
+public class SpikeAssets {
+
+    private static final int FRAME_WIDTH = 16;
+    private static final int FRAME_HEIGHT = 16;
+
+    private final Image[] frames = new Image[2];
+
+    public SpikeAssets() {
+
+        Image sheet = new Image(
+                Objects.requireNonNull(
+                        getClass().getResourceAsStream(
+                                "/image/obstacles/spike.png"
+                        )
+                )
+        );
+
+        frames[0] = new WritableImage(
+                sheet.getPixelReader(),
+                0, 0,
+                FRAME_WIDTH,
+                FRAME_HEIGHT
+        );
+
+        frames[1] = new WritableImage(
+                sheet.getPixelReader(),
+                0, FRAME_HEIGHT,
+                FRAME_WIDTH,
+                FRAME_HEIGHT
+        );
+    }
+
+    public Image getHiddenFrame() {
+        return frames[0];
+    }
+
+    public Image getSpikeFrame() {
+        return frames[1];
+    }
+}
