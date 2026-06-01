68368d998099c9791107f23be53020ca43ca9ea1
Dustin Ivander <dustinduck@duck.com>
2026-06-01 21:19:09 +0700

    Update Player.java

--- src/main/java/io/github/maze/entities/Player.java
+++ src/main/java/io/github/maze/entities/Player.java
@@ -2,5 +2,18 @@ package io.github.maze.entities;
 
 public class Player {
 
+    private int health;
 
+    public void damage(int i) {
+        health -= i;
+
+        if (health <= 0)
+            health = 0;
+    }
+
+//    public int getTileX() {
+//    }
+//
+//    public int getTileY() {
+//    }
 }
