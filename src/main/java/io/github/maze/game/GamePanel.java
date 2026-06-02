8dd8e270e3b8fd2eb6ee3a2f522e841c823328b1
Deastrisk <218586909+Deastrisk@users.noreply.github.com>
2026-06-02 10:14:00 +0700

    added game loop

--- src/main/java/io/github/maze/game/GamePanel.java
+++ src/main/java/io/github/maze/game/GamePanel.java
@@ -5,6 +5,7 @@ package io.github.maze.game;
 *
 */
 
+import io.github.maze.entities.Player;
 import io.github.maze.maze.GameObject;
 import io.github.maze.maze.TileManager;
 import javafx.scene.canvas.Canvas;
@@ -19,31 +20,31 @@ import java.util.List;
 
 public class GamePanel extends Pane implements Runnable {
 
-    public final class Constants {
+    public static final int TILE_SIZE = 16;
+    public static final int SCALE = 2;
 
-        public static final int TILE_SIZE = 16;
-
-        public static final int SCREEN_WIDTH = 800;
-        public static final int SCREEN_HEIGHT = 600;
-
-        private Constants() {}
-    }
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 600;
 
     public List<GameObject> renderBucket = new ArrayList<>();
+
+    public int FPS = 30;
 
     final Game game;
     public final Canvas canvas;
     public final GraphicsContext gc;
     public final TileManager tileManager;
+    public final Player player;
     public Thread gameThread;
 
     public GamePanel(Game game) {
         this.game = game;
 
-        canvas = new Canvas(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+        canvas = new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
         gc = canvas.getGraphicsContext2D();
 
         tileManager = new TileManager();
+        player = new Player();
 
         getChildren().add(canvas);
     }
@@ -55,10 +56,27 @@ public class GamePanel extends Pane implements Runnable {
     // MAIN GAME LOOP
     @Override
     public void run() {
+
+        long drawInterval = 1000000000 / FPS; // (1 sec in nanoseconds) / FPS
+        long delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
         while (true) {
 
-            update();
-            render();
+            currentTime = System.nanoTime();                    // dapetin waktu sekarang dlm nanosecond spy akurat
+            delta += (currentTime - lastTime) / drawInterval;   // jarak waktu antara tiap frame
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                render();
+            }
+
+            try {
+                Thread.sleep(1_000_000_000 / FPS);
+            } catch (InterruptedException e) {
+            }
         }
     }
 
@@ -68,8 +86,8 @@ public class GamePanel extends Pane implements Runnable {
 
         ImageView imageView = new ImageView(grass);
 
-        for (int y = 0; y < Constants.SCREEN_HEIGHT; y += Constants.TILE_SIZE) {
-            for (int x = 0; x < Constants.SCREEN_WIDTH; x += Constants.TILE_SIZE) {
+        for (int y = 0; y < SCREEN_HEIGHT; y += TILE_SIZE) {
+            for (int x = 0; x < SCREEN_WIDTH; x += TILE_SIZE) {
 
                 gc.drawImage(grass, x, y);
             }
@@ -85,7 +103,7 @@ public class GamePanel extends Pane implements Runnable {
         renderBucket.sort(Comparator.comparing(GameObject::getDepth));
         for (int i = 0; i < renderBucket.size(); i++) {
             GameObject o = renderBucket.get(i);
-            o.draw(g);
+            o.render(g);
         }
     }
 
