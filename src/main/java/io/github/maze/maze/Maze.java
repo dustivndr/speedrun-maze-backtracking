7f28d0029b9163df7d8ae1afb61b674f0591023a
BENIMON07 <gredialvino.2007@gmail.com>
2026-06-03 22:26:27 +0700

    added maze

--- src/main/java/io/github/maze/maze/Maze.java
+++ src/main/java/io/github/maze/maze/Maze.java
@@ -1,4 +1,49 @@
 package io.github.maze.maze;
 
+import io.github.maze.game.GamePanel;
+import io.github.maze.obstacles.Hole;
+import io.github.maze.obstacles.Spike;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Maze {
+
+    final GamePanel gp;
+    public List<GameObject> objectList = new ArrayList<>();
+
+    public Maze(GamePanel gp) {
+        this.gp = gp;
+
+        // DEBUG
+//        addObject(2, 1, 1);
+    }
+
+    public void addObject(int id, int col, int row) {
+
+        double x = col * GamePanel.TILE_SIZE;
+        double y = row * GamePanel.TILE_SIZE;
+        switch (id) {
+
+            // just nothing, do not add GameObject
+            case 0 -> {}
+
+            // BushWall
+            case 1 -> {
+                // TODO: commented since BushWall doesn't yet exists
+//                objectList.add(new BushWall(gp, x, y));
+            }
+
+            // Spike
+            case 2 -> {
+                objectList.add(new Spike(gp, x, y));
+            }
+
+            // Hole
+            case 3 -> {
+                // TODO: commented since Hole constructor hasn't yet been made
+//                objectList.add(new Hole(gp, x, y));
+            }
+        }
+    }
 }
