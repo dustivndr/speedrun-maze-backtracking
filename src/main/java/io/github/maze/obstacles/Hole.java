101b1cce49383c57fe2ea5fb4e8bf30034475a09
Deastrisk <218586909+Deastrisk@users.noreply.github.com>
2026-06-06 22:35:49 +0700

    fixed coordinate in Hole constructor

--- src/main/java/io/github/maze/obstacles/Hole.java
+++ src/main/java/io/github/maze/obstacles/Hole.java
@@ -1,8 +1,10 @@
 package io.github.maze.obstacles;
 
+import io.github.maze.entities.Player;
 import io.github.maze.game.GamePanel;
 import io.github.maze.render.Camera;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.shape.Rectangle;
 
 public class Hole extends Obstacle {
 
@@ -14,17 +16,19 @@ public class Hole extends Obstacle {
 
     int animationCounter = 0;
 
+    long lastTime = 0;
+
     public Hole(GamePanel gp, int col, int row) {
         super(gp,
-                col * GamePanel.TILE_SIZE * GamePanel.SCALE,
-                row * GamePanel.TILE_SIZE * GamePanel.SCALE,
+                col * GamePanel.TILE_SIZE,
+                row * GamePanel.TILE_SIZE,
                 GamePanel.TILE_SIZE, GamePanel.TILE_SIZE);
         camera = gp.camera;
     }
 
     @Override
     public void render(GraphicsContext gc) {
-        System.out.println("hole rendered");
+
         if (!hasOpened) return;
         gc.drawImage(holeAssets.getImage(animationCounter), camera.getScreenX(x), camera.getScreenY(y));
     }
@@ -34,8 +38,14 @@ public class Hole extends Obstacle {
 
         int col = (int) (x / GamePanel.TILE_SIZE);
         int row = (int) (y / GamePanel.TILE_SIZE);
-        if (gp.player.getTileX() == col &&
-                gp.player.getTileY() == row) {
+
+        Player p = gp.player;
+        if (
+                p.getX() < x + width &&
+                p.getX() + p.getWidth() > x &&
+                p.getY() < y + height &&
+                p.getY() + p.getHeight() > y
+        ) {
             hasOpened = true;
             collision = true;
         }
