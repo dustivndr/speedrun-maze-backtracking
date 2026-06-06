ad2d8a72e3adbd669f5d0ea0a96b16327dcb33f3
Deastrisk <218586909+Deastrisk@users.noreply.github.com>
2026-06-06 18:56:43 +0700

    finished kunai, fixed player rendering and outofbound check

--- src/main/java/io/github/maze/entities/projectile/KunaiAssets.java
+++ src/main/java/io/github/maze/entities/projectile/KunaiAssets.java
@@ -2,15 +2,22 @@ package io.github.maze.entities.projectile;
 
 import io.github.maze.util.Angle;
 import io.github.maze.util.Util;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 
 public class KunaiAssets {
 
+    private Image image = Util.getScaledImage("/image/ninja/kunai/KUNAI.png");
+
     public Image getRotatedKunai(Angle angle) {
-        Image image = Util.getScaledImage("/image/ninja/kunai/KUNAI.png");
         ImageView imageView = new ImageView(image);
-        imageView.setRotate(angle.getDegrees());
-        return image;
+        imageView.setRotate(angle.getDegrees() + 225);
+
+        SnapshotParameters params = new SnapshotParameters();
+        params.setFill(Color.TRANSPARENT);
+
+        return imageView.snapshot(params, null);
     }
 }
