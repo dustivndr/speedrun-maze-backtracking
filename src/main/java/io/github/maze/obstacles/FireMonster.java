d2dec33eea04e5e30277aa6dfc0b09b0704bd585
Deastrisk <218586909+Deastrisk@users.noreply.github.com>
2026-06-07 22:55:28 +0700

    made firemonster class

---
+++ src/main/java/io/github/maze/obstacles/FireMonster.java
@@ -0,0 +1,24 @@
+package io.github.maze.obstacles;
+
+import io.github.maze.game.GamePanel;
+import javafx.scene.canvas.GraphicsContext;
+
+public class FireMonster extends Obstacle {
+
+    public FireMonster(GamePanel gp, double x, double y) {
+        super(gp, x, y, GamePanel.TILE_SIZE * 2, GamePanel.TILE_SIZE);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+
+    }
+
+    @Override public double getDepth() { return y + height; }
+    @Override public boolean getCollision() { return true; }
+}
